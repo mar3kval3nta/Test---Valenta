79bd08c4a4a6b3410127229c69610a008cf4821b
Marek Valenta <Valenta.Marek@stud.vassboskovice.cz>
2022-05-17 10:33:32 +0200

    Add project files.

---
+++ Test - Valenta/Nakladak.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test___Valenta
+{
+    class Nakladak
+    {
+        public int maxNosnost { get; }
+        public int maxPalivo { get; }
+        public int spotreba { get; }
+        public int palivo { get; set; }
+        public int naklad { get; set; }
+    }
+}
